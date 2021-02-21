4639b82a0f7c3c029bcb7d1d5bd96e40b009845c
jastel99@gmail.com <jastel99@gmail.com>
2021-02-21 23:47:32 +0200

    Add project files.

---
+++ GameTanks/Engine.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Levels.h"
+
+class Engine {
+private:
+
+
+public:
+	Engine();
+
+	void Start();
+};
---
+++ GameTanks/Game.cpp
@@ -0,0 +1,15 @@
+#include "Engine.h"
+
+void RunGame();
+
+
+int main()
+{
+    RunGame();
+}
+
+void RunGame()
+{
+    Engine engine;
+    engine.Start();
+}
