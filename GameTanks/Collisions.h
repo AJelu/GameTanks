29f560bf8625c91cc7ede336a79f0fdaf170dcd8
vovk199420@gmail.com <vovk199420@gmail.com>
2021-02-22 23:23:37 +0200

    Write objects.h and Collisions.h. Structures of base clases. Part 1.

--- GameTanks/Collisions.h
+++ GameTanks/Collisions.h
@@ -2,10 +2,25 @@
 
 class BaseCollision
 {
-
+public:
+	//check if coordinate is in figure
+	virtual bool CoordinatesInFigure(int x_coordinate, int y_coordinate);
 };
 
 class RoundCollision : BaseCollision
 {
+private:
+	int x_, y_, r_;
 
+public:
+	RoundCollision(int x_coordinate, int y_coordinate, int radius);
+
+	int GetCoordinateX();
+	int GetCoordinateY();
+	int GetRadius();
+
+	int SetCoordinate(int x_coordinate, int y_coordinate);
+	int SetRadius(int radius);
+
+	//virtual function
 };
--- GameTanks/Objects.h
+++ GameTanks/Objects.h
@@ -1,27 +1,71 @@
 #pragma once
 #include "Collisions.h"
+#include <string>
+
+using namespace std;
+
 //this objects using in levels to create game expiriens: game menu, game levels
 
 class BaseObjects 
 {
-	//daughter objects?
+protected:
+	//BaseObjects * parent_ = nullptr;
+	int x_, y_;
+	bool level_position; //use level coordinate or screen coordinate
+
+public:
+	virtual string GetClassName();
+	virtual int /*returm message*/ SendAndProcessingMessage(/*mess*/);
+	virtual void /*returm message*/ GetMassages();
+
+	bool Get
 };
 
 class VisibleObject : BaseObjects
 {
-	//size, coordinate, picture, function draw...
+protected:
+	bool need_redraw_image_;
+	bool visible_;
+	int layer_;
+	int height_, width_;
+	//texture;
+public:
+	VisibleObject(int x, int y, int h, int w, /*texture,*/ int layer);
+
+	int GetLayer();
+	int GetCoordinateX();
+	int GetCoordinateY();
+	int GetHeight();
+	int GetWidth();
+	bool NeedRedrawImage();
+	bool Visible();
+
+	int SetLayer(int layer);
+	int SetCoordinate(int x_coordinate, int y_coordinate);
+	int SetSize(int height, int width);
+	int SetTexture(/*texture*/);
+
+	bool Hide();
+	bool Show();
+	bool Draw(/*canvas,*/ int offset_level_x, int offset_level_y);
 };
 
 
 
 class UiObject : VisibleObject
 {
-	
+protected:
+	bool focus_on_this_;
+public:
+	virtual int SendAndProcessingMessage(int mess);
 };
 
 class Button : UiObject
 {
-
+protected:
+	
+public:
+	int SendAndProcessingMessage(int mess);
 };
 
 //others ui objects, and camera
@@ -30,7 +74,7 @@ class Button : UiObject
 
 class GameObject : BaseObjects
 {
-	//colisions
+	//add colisions
 };
 
 
@@ -40,6 +84,8 @@ class StaticObject : GameObject
 };
 
 //others statics objects. tree, stone, brick
+
+
 
 class MovebleObject : GameObject
 {
